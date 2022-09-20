1d5c3a5d5a5e571523709bb04e8aa8c4786cff75
redasSer <redas.serafim@gmail.com>
2022-09-20 20:33:35 +0300

    cleanup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,10 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+
 
 public class Main {
 
